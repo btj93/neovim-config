4844801002a4849332116621d60a46136f79e526
Jeffrey Cheung <jeffrey991110@gmail.com>
2025-09-18 16:05:05 +0900

    feat: upgrade lazyvim

--- lua/plugins/nvim-treesitter.lua
+++ lua/plugins/nvim-treesitter.lua
@@ -1,28 +1,52 @@
 return {
   {
     "nvim-treesitter/nvim-treesitter",
+    config = function()
+      vim.api.nvim_create_autocmd("User", {
+        pattern = "TSUpdate",
+        callback = function()
+          require("nvim-treesitter.parsers").gherkin = {
+            install_info = {
+              url = "https://github.com/binhtran432k/tree-sitter-gherkin",
+              branch = "main", -- only needed if different from default branch
+              queries = "queries/gherkin", -- also install queries from given directory
+            },
+          }
+        end,
+      })
+
+      vim.treesitter.language.register("gherkin", { "feature", "cucumber" })
+      vim.api.nvim_create_autocmd("FileType", {
+        pattern = { "cucumber", "go", "gomod", "javascript", "typescript", "lua", "bash", "markdown" },
+        callback = function()
+          vim.treesitter.start()
+        end,
+      })
+    end,
     opts = function(_, opts)
       -- Register the Gherkin language for .feature files
-      vim.treesitter.language.register("gherkin", "cucumber")
-      local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
-      parser_config.gherkin = {
-        install_info = {
-          url = "https://github.com/binhtran432k/tree-sitter-gherkin",
-          branch = "main",
-          files = { "src/parser.c", "src/scanner.c" },
-        },
-        filetype = "feature",
-      }
+      -- vim.treesitter.language.register("gherkin", "cucumber")
+      -- local parsers = require("nvim-treesitter.parsers")
+      -- parsers.gherkin = {
+      --   install_info = {
+      --     url = "https://github.com/binhtran432k/tree-sitter-gherkin",
+      --     branch = "main",
+      --     revision = "1a709ae",
+      --     path = { "src/parser.c", "src/scanner.c" },
+      --   },
+      --   tier = 2,
+      --   filetype = "feature",
+      -- }
 
-      local gherkin_opts = {
-        highlight = {
-          enable = { "gherkin" },
-
-          additional_vim_regex_highlighting = false,
-        },
-      }
-
-      opts = vim.tbl_deep_extend("force", opts, gherkin_opts)
+      -- local gherkin_opts = {
+      --   highlight = {
+      --     enable = { "gherkin" },
+      --
+      --     additional_vim_regex_highlighting = false,
+      --   },
+      -- }
+      --
+      -- opts = vim.tbl_deep_extend("force", opts, gherkin_opts)
 
       return opts
     end,
