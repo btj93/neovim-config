416fe35b96591bc16aa765b7b184e130427b6372
Jeffrey Cheung <jeffrey991110@gmail.com>
2025-10-27 11:29:09 +0900

    chore: lspconfig keys

--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -1,13 +1,15 @@
 return {
   "neovim/nvim-lspconfig",
-  opts = function(_, opts)
-    -- use vim.diagnostic.config virtual_lines from neovim 0.11
-    opts.diagnostics.virtual_text = false
-    local keys = require("lazyvim.plugins.lsp.keymaps").get()
-    -- change a keymap
-    keys[#keys + 1] = { "K", false }
-    keys[#keys + 1] = { "<leader>ca", false }
-    keys[#keys + 1] = { "m", vim.lsp.buf.hover }
-    return opts
-  end,
+  opts = {
+    servers = {
+      ["*"] = {
+        keys = {
+          { "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", has = "definition" },
+          { "K", false },
+          { "<leader>ca", false }, -- use tiny-code-actions
+          { "m", vim.lsp.buf.hover },
+        },
+      },
+    },
+  },
 }
