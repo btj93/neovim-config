cec7d80d49a022f11e1ef9b3e141f864f589e921
Jeffrey Cheung <jeffrey991110@gmail.com>
2025-11-07 19:13:13 +0900

    chore: lazy lock

--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -7,7 +7,14 @@ return {
           { "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", has = "definition" },
           { "K", false },
           { "<leader>ca", false }, -- use tiny-code-actions
-          { "m", vim.lsp.buf.hover },
+          {
+            "m",
+            function()
+              vim.lsp.buf.hover({
+                border = "single",
+              })
+            end,
+          },
         },
       },
     },
