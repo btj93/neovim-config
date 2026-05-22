d07341e3dd4489070dabf4d61829fb8347def611
Jeffrey Cheung <jeffrey991110@gmail.com>
2026-05-22 13:46:49 +0900

    feat: hotkey

--- lua/plugins/codesnap-nvim.lua
+++ lua/plugins/codesnap-nvim.lua
@@ -3,9 +3,9 @@ return {
   tag = "v2.0.0-beta.17",
   cmd = { "CodeSnap", "CodeSnapSave", "CodeSnapHighlight" },
   keys = {
-    { "cc", "<Esc><cmd>CodeSnap<cr>", mode = "v", desc = "Code snap to clipboard" },
-    { "cs", "<Esc><cmd>CodeSnapSave<cr>", mode = "v", desc = "Code snap to file" },
-    { "ch", "<Esc><cmd>CodeSnapHighlight<cr>", mode = "v", desc = "Code snap highlight" },
+    { "<leader>cc", "<Esc><cmd>CodeSnap<cr>", mode = "v", desc = "Code snap to clipboard" },
+    { "<leader>cs", "<Esc><cmd>CodeSnapSave<cr>", mode = "v", desc = "Code snap to file" },
+    { "<leader>ch", "<Esc><cmd>CodeSnapHighlight<cr>", mode = "v", desc = "Code snap highlight" },
   },
   opts = {},
 }
